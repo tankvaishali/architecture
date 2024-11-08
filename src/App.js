42f4ea8f6361c3d820e58c2b45acb3bcf3a7aa87
Mital-123 <mailto:mittalvaghela2003@gmail.com>
2024-11-08 10:04:59 +0530

    contactus page done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Homepage from './Component/Homepage/Homepage';
 import After from './HOC/After';
 import AboutusPage from './HOC/AboutusPage';
+import ContactUs from './Component/ContactUs/ContactUs';
 
 
 function App() {
@@ -14,9 +15,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Homepage />} />
-          <Route path='/after' element={  <After/>} />
-          <Route path='/aboutus' element={  <AboutusPage/>} />
-      
+          <Route path='/after' element={<After />} />
+          <Route path='/aboutus' element={<AboutusPage />} />
+          <Route path='/contactus' element={<ContactUs />} />
         </Routes>
       </BrowserRouter>
     </>
