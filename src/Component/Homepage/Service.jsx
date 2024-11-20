acc631f84357941ea52549db43d0f654e6f6d144
surbhi <surbhipansuriya633@gmail.com>
2024-11-20 16:29:23 +0530

    service page

---
+++ src/Component/Homepage/Service.jsx
@@ -0,0 +1,98 @@
+import React from 'react'
+import { FaTruck, FaTools, FaWrench, FaRoad, FaMotorcycle, FaTruckMonster, FaCarCrash, FaWarehouse } from 'react-icons/fa';
+
+function Service() {
+    const serviceColumns = [
+        [
+            { name: "LIGHT DUTY TOWING & RECOVERY", icon: <FaTruck /> },
+            { name: "WINCH OUTS", icon: <FaTools /> },
+            { name: "ROADSIDE ASSISTANCE", icon: <FaRoad /> },
+        ],
+        [
+            { name: "MEDIUM DUTY TOWING & RECOVERY", icon: <FaTruck /> },
+            { name: "EQUIPMENT HAULING", icon: <FaWrench /> },
+            { name: "MOTORCYCLE TOWING", icon: <FaMotorcycle /> },
+        ],
+        [
+            { name: "HEAVY DUTY TOWING & TRANSPORT", icon: <FaTruckMonster /> },
+            { name: "ROTATOR & RECOVERY", icon: <FaCarCrash /> },
+            { name: "SPECIALIZED TRANSPORT", icon: <FaWarehouse /> },
+        ],
+        [
+            { name: "EXTREME OFFROAD RECOVERY", icon: <FaTruckMonster /> },
+            { name: "POLICE TOWING & IMPOUND", icon: <FaCarCrash /> },
+            { name: "FORKLIFT SERVICE", icon: <FaTools /> },
+        ],
+    ];
+
+    return (
+        <>
+            <div className='container-lg my-5'>
+                <div className="about-section">
+                    <div className="line"></div>
+                    <span>Our Service</span>
+                </div>
+                <h1 className="Discover text-dark py-2">WHAT WE SERVE</h1>
+                <div className='d-flex justify-content-center flex-wrap'>
+                    {serviceColumns.map((column, Index) => (
+                        <div key={Index} className={`servicesec servnum${Index}`}>
+                            {
+                                column.map((item, i) => (
+                                    <div key={i} className='servmain my-md-1 my-3'>
+                                        <div className="servicebg text-center">
+                                            <div className='servicebox'>
+                                                <p className='servicetext'>
+                                                    <span className='fs-5'>{item.icon}</span>
+                                                    <br />
+                                                    {item.name}
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+        // <div>
+
+        //     <section id="our-services">
+        //     <div className="container">
+        //     <div class="desc ">
+        //                 <div >
+        //                     <div className="about-section ">
+        //                         <div className="line"></div>
+        //                         <span>Our Services</span>
+        //                     </div>
+        //                     <h1 className="Discover text-dark py-2">WHAT WE OFFER</h1>
+        //                 </div>
+        //             </div>
+        //     </div>
+        //         <div class="container-md">
+
+        //             <div className="services">
+        //                 {serviceColumns.map((column, colIndex) => (
+        //                     <div className="column" key={colIndex}>
+        //                         {column.map((service, index) => (
+        //                             <div className="service" key={index}>
+        //                                 <div className="service-desc">
+        //                                     <div className="service-icon service-txt">{service.icon}</div>
+        //                                     <p className="service-txt mt-3">{service.name}</p>
+        //                                 </div>
+        //                             </div>
+        //                         ))}
+        //                     </div>
+        //                 ))}
+        //             </div>
+        //             <div class="clear-fix"></div>
+        //         </div>
+
+        //     </section>
+        // </div>
+
+    )
+}
+
+export default Service
