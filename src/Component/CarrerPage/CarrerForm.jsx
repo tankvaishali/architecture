f010e99c28cf5c2dc78f135053fc78cf9d3c7662
tankvaishali <vaishalitank28603@gmail.com>
2024-11-27 10:12:12 +0530

    career page changes

--- src/Component/CarrerPage/CarrerForm.jsx
+++ src/Component/CarrerPage/CarrerForm.jsx
@@ -5,12 +5,24 @@ import { HiOutlineArrowSmRight } from "react-icons/hi";
 import { IoMdMail, IoMdPerson } from "react-icons/io";
 import { MdOutlineSubject } from "react-icons/md";
 import { SiBuzzfeed } from "react-icons/si";
+import videolink from "../../Assets/Video/3135929-hd_1920_1080_25fps.mp4"
 
 function CarrerForm() {
   return (
     <>
       <div className="my-5">
         <div className="carrer_form_bg p-0 p-lg-5">
+          <video
+            className="carrer_video_bg"
+            autoPlay
+            loop
+            muted
+            playsInline
+          >
+            <source src={videolink} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+
           <div className="container">
             <div className="row p-4">
               <div className="col-12 col-lg-10 mx-auto">
@@ -21,14 +33,14 @@ function CarrerForm() {
                     data-aos-duration="1500"
                     data-aos-once="true"
                   >
-                    <IoMdPerson className="fs-5" />{" "}
-                    <label htmlFor="" className="ms-1">
+                    <IoMdPerson className="fs-4 fw-bold text-white" />{" "}
+                    <label htmlFor="" className="ms-1 fw-bold text-white fs-6">
                       Name
                     </label>
                     <input
                       type="text"
                       name="name"
-                      className="inpText w-100 d-block border border-dark border-1 px-3 py-2 shadow mt-1 mb-2"
+                      className="inpText text-white w-100 d-block px-3 py-2 shadow  my-2"
                     />
                   </div>
                   <div
@@ -37,14 +49,14 @@ function CarrerForm() {
                     data-aos-duration="1500"
                     data-aos-once="true"
                   >
-                    <IoMdMail className="fs-5" />{" "}
-                    <label className="ms-1" htmlFor="">
+                    <IoMdMail className="fs-4 fw-bold text-white" />{" "}
+                    <label className="ms-1 fw-bold text-white fs-6" htmlFor="">
                       E-mail
                     </label>
                     <input
                       type="email"
                       name="email"
-                      className="inpText w-100 d-block border border-dark border-1 px-3 py-2 shadow mt-1 mb-2"
+                      className="inpText text-white w-100 d-block px-3 py-2 shadow my-2"
                     />
                   </div>
                 </div>
@@ -56,14 +68,14 @@ function CarrerForm() {
                     data-aos-once="true"
                     data-aos-delay="400"
                   >
-                    <BsPhoneFill className="fs-5" />{" "}
-                    <label className="ms-1" htmlFor="">
+                    <BsPhoneFill className="fs-4 fw-bold text-white" />{" "}
+                    <label className="ms-1 fw-bold text-white fs-6" htmlFor="">
                       Phone No.
                     </label>
                     <input
                       type="number"
                       name="phoneNo"
-                      className="inpText w-100 d-block border border-dark border-1 px-3 py-2 shadow mt-1 mb-2"
+                      className="inpText text-white w-100 d-block px-3 py-2 shadow my-2"
                     />
                   </div>
                   <div
@@ -73,14 +85,14 @@ function CarrerForm() {
                     data-aos-once="true"
                     data-aos-delay="400"
                   >
-                    <SiBuzzfeed className="fs-5" />{" "}
-                    <label className="ms-1" htmlFor="">
+                    <SiBuzzfeed className="fs-4 fw-bold text-white" />{" "}
+                    <label className="ms-1 fw-bold text-white fs-6" htmlFor="">
                       Experience
                     </label>
                     <input
                       type="number"
                       name="experience"
-                      className="inpText w-100 d-block border border-dark border-1 px-3 py-2 shadow mt-1 mb-2"
+                      className="inpText text-white w-100 d-block px-3 py-2 shadow my-2"
                     />
                   </div>
                 </div>
@@ -92,14 +104,14 @@ function CarrerForm() {
                     data-aos-once="true"
                     data-aos-delay="600"
                   >
-                    <MdOutlineSubject className="fs-5" />{" "}
-                    <label className="ms-1" htmlFor="">
+                    <MdOutlineSubject className="fs-4 fw-bold text-white" />{" "}
+                    <label className="ms-1 fw-bold text-white fs-6" htmlFor="">
                       Subject
                     </label>
                     <input
                       type="text"
                       name="subject"
-                      className="inpText w-100 d-block border border-dark border-1 px-3 py-2 shadow mt-1 mb-2"
+                      className="inpText text-white w-100 d-block px-3 py-2 shadow my-2"
                     />
                   </div>
                   <div
@@ -109,14 +121,14 @@ function CarrerForm() {
                     data-aos-once="true"
                     data-aos-delay="600"
                   >
-                    <BsFillFileImageFill className="fs-5" />{" "}
-                    <label className="ms-1" htmlFor="">
+                    <BsFillFileImageFill className="fs-4 fw-bold text-white" />{" "}
+                    <label className="ms-1 fw-bold text-white fs-6" htmlFor="">
                       Profile
                     </label>
                     <input
                       type="file"
                       name="profile"
-                      className="inpText w-100 d-block border border-dark border-1 px-3 py-2 shadow mt-1 mb-2"
+                      className="inpText text-white w-100 d-block px-3 py-2 shadow my-2"
                     />
                   </div>
                 </div>
@@ -127,13 +139,13 @@ function CarrerForm() {
                   data-aos-once="true"
                   data-aos-delay="800"
                 >
-                  <AiFillMessage className="fs-5" />{" "}
-                  <label className="ms-1" htmlFor="">
+                  <AiFillMessage className="fs-4 fw-bold text-white" />{" "}
+                  <label className="ms-1 fw-bold text-white fs-6" htmlFor="">
                     Message
                   </label>
                   <textarea
                     name="message"
-                    className="inpText w-100 d-block border border-dark border-1 px-3 py-2 shadow mt-1 mb-2"
+                    className="inpText text-white w-100 d-block px-3 py-2 shadow my-2"
                     id=""
                   ></textarea>
                 </div>
@@ -147,7 +159,7 @@ function CarrerForm() {
                 >
                   <button
                     type="button"
-                    className="bg_button Inter text-white fs-6 ps-4 pe-5 py-2 fw-bold position-relative"
+                    className="bg_button Inter text-white fs-6 ps-4 pe-5 py-2 fw-bold text-white position-relative"
                   >
                     Submit
                     <span className="bg_iconbtn py-3 px-2 text-dark position-absolute end-0">
