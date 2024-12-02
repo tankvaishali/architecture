74a376a239cdc4931700429a9cf6cc3b1e36ea74
tankvaishali <vaishalitank28603@gmail.com>
2024-12-02 11:13:39 +0530

    naub section

--- src/HOC/MainSection.jsx
+++ src/HOC/MainSection.jsx
@@ -3,19 +3,18 @@ import { MdDoubleArrow } from 'react-icons/md'
 
 function MainSection() {
     return (
-<div className='maincontentimg'>
-<div className='mainsection d-flex align-items-center'>
-            <div className='container'>
-                <div className="bg-dark border border-5 border-light col-lg-5 col-md-8 col-9 d-flex align-items-center justify-content-center text-center mainsecbg">
-                    <div className='text-light'>
-                        <h1>ABOUT US</h1>
-                        <p>home <MdDoubleArrow /> about us <MdDoubleArrow /> </p>
+        <div className='maincontentimg'>
+            <div className='mainsection d-flex align-items-center '>
+                <div className='container'>
+                    <div className="bg-dark border border-5 border-light col-lg-5 col-md-8 col-9 d-flex align-items-center justify-content-center text-center mainsecbg">
+                        <div className='text-light'>
+                            <h1>ABOUT US</h1>
+                            <p>home <MdDoubleArrow /> about us <MdDoubleArrow /> </p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div >
-
-</div>
+        </div>
     )
 }
 
