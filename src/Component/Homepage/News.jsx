f2ca81772a7928b5d4aec00e0fe5747880eeb744
tankvaishali <vaishalitank28603@gmail.com>
2024-12-02 10:14:40 +0530

    lending page section

--- src/Component/Homepage/News.jsx
+++ src/Component/Homepage/News.jsx
@@ -1,6 +1,7 @@
 import { ArrowUpRight } from 'lucide-react'
 import { HiOutlineArrowSmRight } from 'react-icons/hi'
 import videolink from "../../Assets/Video/contact_architecture.mp4"
+import { Link } from 'react-router-dom'
 
 
 export default function News() {
@@ -43,7 +44,8 @@ export default function News() {
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                                 eiusmod tempor incididunt ut labore et dolore magna aliqua.
                             </p>
-                            <button
+                         <Link className='text-decoration-none' to={"/contactus"}>
+                         <button
                                           data-aos="fade-right" data-aos-duration="1500" data-aos-once="true"
                                 type="button"
                                 className="bg_button text-white Inter fs-6 ps-4 pe-5 py-2 fw-bold  position-relative">
@@ -52,10 +54,11 @@ export default function News() {
                                     <HiOutlineArrowSmRight className="fs-5" />
                                 </span>
                             </button>
+                         </Link>
                         </div>
                         <div className="news-list">
                             {newsItems.map((item, index) => (
-                                <a key={index} href="#" className="news-item" data-aos="fade-left" data-aos-duration="1500" data-aos-once="true" data-aos-delay={item.delay}>
+                                <Link key={index} to={"/"} className="news-item" data-aos="fade-left" data-aos-duration="1500" data-aos-once="true" data-aos-delay={item.delay}>
                                     <div>
                                         <span className="text-white fw-bold fs-5 me-4">{item.category}</span>
                                         <span className="fs-6 text-white fw-bold fs-5">{item.date}</span>
@@ -64,7 +67,7 @@ export default function News() {
                                         <h2 className='fs-6 lh-lg category'>{item.title}</h2>
                                         <ArrowUpRight className="arrow-icon text-white" />
                                     </div>
-                                </a>
+                                </Link>
                             ))}
                         </div>
                     </div>
--- src/HOC/MainSection.jsx
+++ src/HOC/MainSection.jsx
@@ -3,7 +3,8 @@ import { MdDoubleArrow } from 'react-icons/md'
 
 function MainSection() {
     return (
-        <div className='mainsection d-flex align-items-center'>
+<div className='maincontentimg'>
+<div className='mainsection d-flex align-items-center'>
             <div className='container'>
                 <div className="bg-dark border border-5 border-light col-lg-5 col-md-8 col-9 d-flex align-items-center justify-content-center text-center mainsecbg">
                     <div className='text-light'>
@@ -13,6 +14,8 @@ function MainSection() {
                 </div>
             </div>
         </div >
+
+</div>
     )
 }
 
